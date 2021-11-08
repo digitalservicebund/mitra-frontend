18006f7b4d3356ba8737a5f6287ffc4e651c06fc
Klaus Hartl <klaus.hartl@gmail.com>
2021-11-08 11:43:44 +0100

    Add CSP in Vite's production mode only
    Follow-up on e76cfd4f50fb9725853202bae0dbfe079709dd08
    
    This approach using Handlebars is a. more intention revealing and b.
    more importantly does not weaken the XSS protection.

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,10 @@
 import { defineConfig } from "vite"
 import vue from "@vitejs/plugin-vue"
+import handlebars from "vite-plugin-handlebars"
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()],
+  plugins: [vue(), handlebars()],
   base: "/mitra-frontend/",
   server: {
     watch: {
