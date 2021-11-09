e583de0b4b16ff6cecc2e77382552376e2fa1457
Klaus Hartl <klaus.hartl@gmail.com>
2021-11-09 15:27:35 +0100

    Fill in content for start screen

--- src/pages/index.vue
+++ src/pages/index.vue
@@ -5,7 +5,17 @@
 <template>
   <Header />
   <section>
-    <router-link to="/mitra-frontend/contract">Neuer Vertrag</router-link>
+    <h2>Verträge</h2>
+    <p>Vertrag bearbeiten</p>
+    <p>
+      <router-link to="/mitra-frontend/contract">Neuer Vertrag</router-link>
+    </p>
+    <p>Neuer Vertrag aus lokaler Datei</p>
+  </section>
+  <section>
+    <h2>Playbooks</h2>
+    <p>Playbook bearbeiten</p>
+    <p>Neues Playbook</p>
   </section>
 </template>
 
