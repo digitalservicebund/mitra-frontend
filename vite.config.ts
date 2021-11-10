71e19acbe2b778e27d1a164bdc623979d31bf997
Klaus Hartl <klaus.hartl@gmail.com>
2021-11-10 12:14:29 +0100

    Ignore lighthouse artefacts everywhere

--- vite.config.ts
+++ vite.config.ts
@@ -15,7 +15,7 @@ export default defineConfig({
   base: "/mitra-frontend/",
   server: {
     watch: {
-      ignored: ["**/coverage/**"],
+      ignored: ["**/coverage/**", "**/.lighthouseci/**"],
     },
   },
 })
