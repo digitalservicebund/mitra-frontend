6e2e0e28b26dbadc848b3967699dcbdf44f71e26
Klaus Hartl <klaus.hartl@gmail.com>
2021-11-10 18:30:43 +0100

    Finish cards in contract screen
    - Add Material icons
    - Work out alignment

--- src/pages/index.vue
+++ src/pages/index.vue
@@ -7,15 +7,55 @@
   <section>
     <h2 class="font-black mt-8 mb-2">Verträge</h2>
     <div class="flex flex-row">
-      <p class="h-48 w-48 border p-4 mr-2 opacity-60">Vertrag bearbeiten</p>
-      <p class="flex h-48 w-48 mr-2">
-        <router-link
-          to="/mitra-frontend/contract"
-          class="flex-1 p-4 border hover:bg-gray-50 focus:bg-gray-50"
-          >Neuer Vertrag</router-link
+      <p class="flex flex-col w-48 mr-2 opacity-60">
+        <span
+          class="
+            flex flex-col
+            items-center
+            justify-center
+            h-48
+            material-icons-outlined
+            border
+            text-6xl
+          "
+          aria-hidden="true"
+          >description</span
         >
+        Vertrag bearbeiten
       </p>
-      <p class="h-48 w-48 border p-4 opacity-60">
+      <p class="flex flex-col w-48 mr-2">
+        <router-link to="/mitra-frontend/contract" class="block"
+          ><span
+            class="
+              flex flex-col
+              items-center
+              justify-center
+              h-48
+              material-icons-outlined
+              border
+              text-6xl
+              hover:bg-gray-50
+            "
+            aria-hidden="true"
+            >cloud</span
+          >
+          Neuer Vertrag
+        </router-link>
+      </p>
+      <p class="flex flex-col w-48 mr-2 opacity-60">
+        <span
+          class="
+            flex flex-col
+            items-center
+            justify-center
+            h-48
+            material-icons-outlined
+            border
+            text-6xl
+          "
+          aria-hidden="true"
+          >add</span
+        >
         Neuer Vertrag aus lokaler Datei
       </p>
     </div>
@@ -23,8 +63,38 @@
   <section>
     <h2 class="font-black mt-8 mb-2">Playbooks</h2>
     <div class="flex flex-row">
-      <p class="h-48 w-48 border p-4 mr-2 opacity-60">Playbook bearbeiten</p>
-      <p class="h-48 w-48 border p-4 opacity-60">Neues Playbook</p>
+      <p class="flex flex-col w-48 mr-2 opacity-60">
+        <span
+          class="
+            flex flex-col
+            items-center
+            justify-center
+            h-48
+            material-icons-outlined
+            border
+            text-6xl
+          "
+          aria-hidden="true"
+          >smart_display</span
+        >
+        Playbook bearbeiten
+      </p>
+      <p class="flex flex-col w-48 mr-2 opacity-60">
+        <span
+          class="
+            flex flex-col
+            items-center
+            justify-center
+            h-48
+            material-icons-outlined
+            border
+            text-6xl
+          "
+          aria-hidden="true"
+          >add</span
+        >
+        Neues Playbook
+      </p>
     </div>
   </section>
 </template>
