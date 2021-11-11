d800282d411da6794a54db03b109d759d396651e
Klaus Hartl <klaus.hartl@gmail.com>
2021-11-11 06:50:49 +0100

    Link "logo" to start screen
    Allow for more easily getting back...

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,5 +1,7 @@
 <template>
   <header class="py-4 text-xl">
-    <h1 class="font-black">Mitra</h1>
+    <h1 class="font-black">
+      <router-link to="/mitra-frontend/">Mitra</router-link>
+    </h1>
   </header>
 </template>
