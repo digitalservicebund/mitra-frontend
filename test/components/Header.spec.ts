14f6ce5e26740299272ea096dc2d8c338091e158
Klaus Hartl <klaus.hartl@gmail.com>
2021-11-11 08:02:20 +0100

    Eliminate warning in jest unit test
    Caused by adding `router-link` to the component. We need to provide a
    router for rendering the component under test with the expected text.
    That's quite a bit of setup for such a small component.

--- test/components/Header.spec.ts
+++ test/components/Header.spec.ts
@@ -1,9 +1,31 @@
 import { render, screen } from "@testing-library/vue"
+import { createRouter, createWebHistory } from "vue-router"
 import Header from "../../src/components/Header.vue"
 
 describe("Header", () => {
-  it("should display header text", () => {
-    render(Header)
+  const router = createRouter({
+    history: createWebHistory(),
+    routes: [
+      {
+        path: "/mitra-frontend/",
+        component: {
+          template: "<div></div>",
+        },
+      },
+    ],
+  })
+
+  beforeAll(async () => {
+    router.push("/mitra-frontend/")
+    await router.isReady()
+  })
+
+  it("should display header text", async () => {
+    render(Header, {
+      global: {
+        plugins: [router],
+      },
+    })
     expect(screen.getByText("Mitra")).toBeVisible()
   })
 })
