91ee729332a7c0849f4ef19cdf5bd26968ef4494
Klaus Hartl <klaus.hartl@gmail.com>
2021-11-11 07:10:28 +0100

    Center content in start screen

--- src/pages/index.vue
+++ src/pages/index.vue
@@ -4,50 +4,52 @@
 
 <template>
   <Header />
-  <section>
-    <h2 class="font-black mt-8 mb-2">Verträge</h2>
-    <div class="flex flex-row">
-      <p class="card opacity-60">
-        <span class="card-icon material-icons-outlined" aria-hidden="true"
-          >description</span
-        >
-        Vertrag bearbeiten
-      </p>
-      <p class="card">
-        <router-link to="/mitra-frontend/contract" class="block"
-          ><span
-            class="card-icon material-icons-outlined hover:bg-gray-50"
-            aria-hidden="true"
-            >cloud</span
+  <main class="grid grid-rows-2 justify-center">
+    <section>
+      <h2 class="font-black mt-8 mb-2">Verträge</h2>
+      <div class="flex flex-row">
+        <p class="card opacity-60">
+          <span class="card-icon material-icons-outlined" aria-hidden="true"
+            >description</span
           >
-          Neuer Vertrag
-        </router-link>
-      </p>
-      <p class="card opacity-60">
-        <span class="card-icon material-icons-outlined" aria-hidden="true"
-          >add</span
-        >
-        Neuer Vertrag aus lokaler Datei
-      </p>
-    </div>
-  </section>
-  <section>
-    <h2 class="font-black mt-8 mb-2">Playbooks</h2>
-    <div class="flex flex-row">
-      <p class="card opacity-60">
-        <span class="card-icon material-icons-outlined" aria-hidden="true"
-          >smart_display</span
-        >
-        Playbook bearbeiten
-      </p>
-      <p class="card opacity-60">
-        <span class="card-icon material-icons-outlined" aria-hidden="true"
-          >add</span
-        >
-        Neues Playbook
-      </p>
-    </div>
-  </section>
+          Vertrag bearbeiten
+        </p>
+        <p class="card">
+          <router-link to="/mitra-frontend/contract" class="block"
+            ><span
+              class="card-icon material-icons-outlined hover:bg-gray-50"
+              aria-hidden="true"
+              >cloud</span
+            >
+            Neuer Vertrag
+          </router-link>
+        </p>
+        <p class="card opacity-60">
+          <span class="card-icon material-icons-outlined" aria-hidden="true"
+            >add</span
+          >
+          Neuer Vertrag aus lokaler Datei
+        </p>
+      </div>
+    </section>
+    <section>
+      <h2 class="font-black mt-8 mb-2">Playbooks</h2>
+      <div class="flex flex-row">
+        <p class="card opacity-60">
+          <span class="card-icon material-icons-outlined" aria-hidden="true"
+            >smart_display</span
+          >
+          Playbook bearbeiten
+        </p>
+        <p class="card opacity-60">
+          <span class="card-icon material-icons-outlined" aria-hidden="true"
+            >add</span
+          >
+          Neues Playbook
+        </p>
+      </div>
+    </section>
+  </main>
 </template>
 
 <style>
