5d750806b44b9ec1012194cdda22d9ca8e8b0c8f
Klaus Hartl <klaus.hartl@gmail.com>
2021-11-11 06:42:08 +0100

    Extract shared component classes in page

--- src/pages/index.vue
+++ src/pages/index.vue
@@ -7,53 +7,24 @@
   <section>
     <h2 class="font-black mt-8 mb-2">Verträge</h2>
     <div class="flex flex-row">
-      <p class="flex flex-col w-48 mr-2 opacity-60">
-        <span
-          class="
-            flex flex-col
-            items-center
-            justify-center
-            h-48
-            material-icons-outlined
-            border
-            text-6xl
-          "
-          aria-hidden="true"
+      <p class="card opacity-60">
+        <span class="card-icon material-icons-outlined" aria-hidden="true"
           >description</span
         >
         Vertrag bearbeiten
       </p>
-      <p class="flex flex-col w-48 mr-2">
+      <p class="card">
         <router-link to="/mitra-frontend/contract" class="block"
           ><span
-            class="
-              flex flex-col
-              items-center
-              justify-center
-              h-48
-              material-icons-outlined
-              border
-              text-6xl
-              hover:bg-gray-50
-            "
+            class="card-icon material-icons-outlined hover:bg-gray-50"
             aria-hidden="true"
             >cloud</span
           >
           Neuer Vertrag
         </router-link>
       </p>
-      <p class="flex flex-col w-48 mr-2 opacity-60">
-        <span
-          class="
-            flex flex-col
-            items-center
-            justify-center
-            h-48
-            material-icons-outlined
-            border
-            text-6xl
-          "
-          aria-hidden="true"
+      <p class="card opacity-60">
+        <span class="card-icon material-icons-outlined" aria-hidden="true"
           >add</span
         >
         Neuer Vertrag aus lokaler Datei
@@ -63,34 +34,14 @@
   <section>
     <h2 class="font-black mt-8 mb-2">Playbooks</h2>
     <div class="flex flex-row">
-      <p class="flex flex-col w-48 mr-2 opacity-60">
-        <span
-          class="
-            flex flex-col
-            items-center
-            justify-center
-            h-48
-            material-icons-outlined
-            border
-            text-6xl
-          "
-          aria-hidden="true"
+      <p class="card opacity-60">
+        <span class="card-icon material-icons-outlined" aria-hidden="true"
           >smart_display</span
         >
         Playbook bearbeiten
       </p>
-      <p class="flex flex-col w-48 mr-2 opacity-60">
-        <span
-          class="
-            flex flex-col
-            items-center
-            justify-center
-            h-48
-            material-icons-outlined
-            border
-            text-6xl
-          "
-          aria-hidden="true"
+      <p class="card opacity-60">
+        <span class="card-icon material-icons-outlined" aria-hidden="true"
           >add</span
         >
         Neues Playbook
@@ -98,3 +49,14 @@
     </div>
   </section>
 </template>
+
+<style>
+  @layer components {
+    .card {
+      @apply flex flex-col w-48 mr-2;
+    }
+    .card-icon {
+      @apply flex flex-col items-center justify-center h-48 border text-6xl;
+    }
+  }
+</style>
