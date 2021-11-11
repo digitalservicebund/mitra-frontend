3f03247c0bdfd0528c337436e24e038859ffcbf5
Klaus Hartl <klaus.hartl@gmail.com>
2021-11-11 07:16:13 +0100

    Fix e2e test after adding link in header
    Making assertion more specific, though the test was never meant to stay
    like this in the first place.

--- test/e2e/contract.spec.ts
+++ test/e2e/contract.spec.ts
@@ -1,8 +1,8 @@
 describe("Starting new contract", () => {
   it("should be reachable from start page", () => {
     cy.visit("/")
-    cy.get("a").contains("Neuer Vertrag").click()
-    cy.get("a").should("not.exist") // navigated away
+    cy.get("main a").contains("Neuer Vertrag").click()
+    cy.get("main a").should("not.exist") // navigated away
     cy.contains("Abschnitt 1")
     cy.contains("Abschnitt 2")
   })
