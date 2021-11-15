be740f07a95c9091b81edd293e5e96eca214b70a
Shahrooz Afsharipour <shahrooz.afsharipour@digitalservice.bund.de>
2021-11-15 16:42:42 +0100

    Add PrimeVue.

--- src/main.ts
+++ src/main.ts
@@ -2,6 +2,7 @@ import { createApp } from "vue"
 import { createRouter, createWebHistory } from "vue-router"
 import routes from "virtual:generated-pages"
 import App from "./App.vue"
+import PrimeVue from "primevue/config"
 import "./index.css"
 
 const router = createRouter({
@@ -9,4 +10,4 @@ const router = createRouter({
   routes,
 })
 
-createApp(App).use(router).mount("#app")
+createApp(App).use(router).use(PrimeVue).mount("#app")
