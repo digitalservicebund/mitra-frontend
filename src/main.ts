2b797299e875c5513179d4a2dae9512bbaa6d8f9
Florian <flo@mzechmeister.de>
2021-11-15 18:13:46 +0100

    Use double quotes

--- src/main.ts
+++ src/main.ts
@@ -5,9 +5,9 @@ import App from "./App.vue"
 import PrimeVue from "primevue/config"
 import "./index.css"
 
-import 'primevue/resources/themes/tailwind-light/theme.css';
-import 'primevue/resources/primevue.min.css';
-import 'primeicons/primeicons.css';
+import "primevue/resources/themes/tailwind-light/theme.css"
+import "primevue/resources/primevue.min.css"
+import "primeicons/primeicons.css"
 
 const router = createRouter({
   history: createWebHistory(),
