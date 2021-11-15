6df741cb2cbda210672b5783018992b992cf7cda
Florian <flo@mzechmeister.de>
2021-11-15 18:08:10 +0100

    Use tailwind-light theme from PrimeVue.

--- src/main.ts
+++ src/main.ts
@@ -5,6 +5,10 @@ import App from "./App.vue"
 import PrimeVue from "primevue/config"
 import "./index.css"
 
+import 'primevue/resources/themes/tailwind-light/theme.css';
+import 'primevue/resources/primevue.min.css';
+import 'primeicons/primeicons.css';
+
 const router = createRouter({
   history: createWebHistory(),
   routes,
