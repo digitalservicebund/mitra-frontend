b69e614d56339a071737b09f10c324677886adcf
Klaus Hartl <klaus.hartl@gmail.com>
2021-11-17 17:38:32 +0100

    Set up purgecss to remove unused css
    Tailwind takes care of itself, but we want to ensure that our own
    theme.css (currently copypasted as a whole from PrimeVue's
    "tailwind-light" theme) doesn't ship with styles for components we don't
    use.
    
    Snapshot of some numbers:
    
    Before purging:
    dist/assets/index.3580866a.css 150.07 KiB / gzip: 19.91 KiB
    
    After purging:
    dist/assets/index.e69e69a6.css 9.44 KiB / gzip: 3.14 KiB
    
    Addresses MIT-88

--- postcss.config.js
+++ postcss.config.js
@@ -1,6 +1,14 @@
+const purgecss = require("@fullhuman/postcss-purgecss")
+const tailwindcss = require("tailwindcss")
+const autoprefixer = require("autoprefixer")
+
 module.exports = {
-  plugins: {
-    tailwindcss: {},
-    autoprefixer: {},
-  },
+  plugins: [
+    purgecss({
+      content: ["./index.html", "./src/**/*.vue"],
+      css: ["./src/theme.css"],
+    }),
+    tailwindcss(),
+    autoprefixer(),
+  ],
 }
