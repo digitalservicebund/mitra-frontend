58bb4a03252a3633b1dd5ef3d3cab55f18275875
shrzaf <shrzaf@users.noreply.github.com>
2021-11-17 17:04:03 +0000

    deploy: 0984108d7e4be12965bfe95b0e2a55b025a4f3ee

--- assets/AppHeader.d1c3db1c.js
+++
@@ -1,1 +0,0 @@
-import{_ as t}from"./index.5cd5d89e.js";import{r as o,o as r,f as s,g as a,i as c,w as n,j as _}from"./vendor.f107a2ca.js";const d={},i={class:"py-4 text-xl"},f={class:"font-black"},l=_("Mitra");function p(m,x){const e=o("router-link");return r(),s("header",i,[a("h1",f,[c(e,{to:"/mitra-frontend/"},{default:n(()=>[l]),_:1})])])}var u=t(d,[["render",p]]);export{u as A};
