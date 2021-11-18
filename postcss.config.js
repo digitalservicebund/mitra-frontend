a46262dde1534c8c92dfef5a32af63618fe6b65c
Klaus Hartl <klaus.hartl@gmail.com>
2021-11-18 11:35:20 +0100

    Enhance PostCSS config
    Adding missing Card component and safelist and extractor suited for
    Vue.js, taken from https://purgecss.com/guides/vue.html#usage
    
    Addresses MIT-88

--- postcss.config.js
+++ postcss.config.js
@@ -8,10 +8,27 @@ module.exports = {
       content: [
         "./index.html",
         "./src/**/*.vue",
+        "./node_modules/primevue/card/Card.vue",
         "./node_modules/primevue/dialog/Dialog.vue",
         "./node_modules/primevue/inputtext/InputText.vue",
       ],
       css: ["./src/theme.css"],
+      defaultExtractor(content) {
+        const contentWithoutStyleBlocks = content.replace(
+          /<style[^]+?<\/style>/gi,
+          ""
+        )
+        return (
+          contentWithoutStyleBlocks.match(/[A-Za-z0-9-_/:]*[A-Za-z0-9-_/]+/g) ||
+          []
+        )
+      },
+      safelist: [
+        /-(leave|enter|appear)(|-(to|from|active))$/,
+        /^(?!(|.*?:)cursor-move).+-move$/,
+        /^router-link(|-exact)-active$/,
+        /data-v-.*/,
+      ],
     }),
     tailwindcss(),
     autoprefixer(),
