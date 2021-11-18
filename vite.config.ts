c4c9e53fefbeea422d6d24385939e2f58fd120fc
Klaus Hartl <klaus.hartl@gmail.com>
2021-11-18 17:15:57 +0100

    Remove no longer required handlebars from vite

--- vite.config.ts
+++ vite.config.ts
@@ -1,13 +1,11 @@
 import { defineConfig } from "vite"
 import vue from "@vitejs/plugin-vue"
-import handlebars from "vite-plugin-handlebars"
 import pages from "vite-plugin-pages"
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
     vue(),
-    handlebars(),
     pages({
       pagesDir: [{ dir: "src/routes", baseRoute: "mitra-frontend" }],
     }),
