a27e94a94c896963bdc0c99b28a521b62c77cd33
Klaus Hartl <klaus.hartl@gmail.com>
2021-11-19 12:32:12 +0100

    Harden PurgeCSS content lookup
    We had been collecting content based on the import statement such as
    
    import Card from "primevue/card"
           ^^^^      ^^^^^^^^^^^^^^^
    
    The name of the import ("Card") would have had to match what the file is
    named in the PrimeVue component ("Card.vue"), but can be freely choosen
    by a developer. This changes the lookup to rely solely on the path part
    ("primevue/card"), which is fix and relyable. It allows developers to
    choose a named import to their liking/needs.
    
    Addresses MIT-88

--- collect-purgecss-content.js
+++ collect-purgecss-content.js
@@ -5,21 +5,29 @@ const fs = require("fs")
 const { exit } = require("process")
 const glob = require("glob")
 
+const primeVueComponentsDir = "./node_modules/primevue"
+if (!fs.existsSync(primeVueComponentsDir)) {
+  console.log(`Specified directory: ${primeVueComponentsDir} does not exist`)
+  exit(1)
+}
+
 const componentsDir = "src/components"
 if (!fs.existsSync(componentsDir)) {
   console.log(`Specified directory: ${componentsDir} does not exist`)
   exit(1)
 }
 
-const componentsPaths = glob.sync("src/components/**/*.vue")
-const regexp = /import (.+) from "primevue\/(.+)"/g
-const nodeModulePaths = componentsPaths
+const importStatementRegexp = /import .+ from "primevue\/(.+)"/g
+const lookup = glob
+  .sync(`${componentsDir}/**/*.vue`)
   .map((path) => fs.readFileSync(path, "utf8"))
-  .map((content) => [...content.matchAll(regexp)])
+  .map((content) => [...content.matchAll(importStatementRegexp)])
   .reduce((memo, current) => memo.concat(current), [])
-  .map((match) => match[0])
-  .map((statement) =>
-    statement.replace(regexp, "./node_modules/primevue/$2/$1.vue")
-  )
+  .map((match) => match[1])
+
+const pathPartRegexp = new RegExp(`/(${lookup.join("|")})/`)
+const nodeModulePaths = glob
+  .sync(`${primeVueComponentsDir}/**/*.vue`)
+  .filter((path) => pathPartRegexp.test(path)) // path must include one of our lookup...
 
 module.exports = nodeModulePaths
