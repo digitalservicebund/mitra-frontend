0dcab107b5b022873f850a327429bfd43a198493
carhartl <carhartl@users.noreply.github.com>
2021-11-23 15:04:04 +0000

    deploy: 33e35ba8955e81454e869fa3dcab8a44358fc6f1

---
+++ assets/AppHeader.2a653f1f.js
@@ -0,0 +1,1 @@
+import{_ as t}from"./index.fdd02ce1.js";import{r as o,o as r,f as s,h as a,l as c,m as n,p as _}from"./vendor.2c191b43.js";const d={},i={class:"py-4 text-xl"},l={class:"font-black"},f=_("Mitra");function p(m,h){const e=o("router-link");return r(),s("header",i,[a("h1",l,[c(e,{to:"/mitra-frontend/"},{default:n(()=>[f]),_:1})])])}var u=t(d,[["render",p]]);export{u as A};
