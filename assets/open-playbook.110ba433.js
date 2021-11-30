03bdfd8aec22c960fd6d23e98fcf3970245b8110
carhartl <carhartl@users.noreply.github.com>
2021-11-30 16:36:43 +0000

    deploy: c430adffce37f6c2d58a0b256c9b06b26b6d834d

---
+++ assets/open-playbook.110ba433.js
@@ -0,0 +1,1 @@
+import{_ as n}from"./index.3f2a18ea.js";import{o as e,e as o,f as t,c as s}from"./vendor.15bef71b.js";const a={};function _(r,c){return e(),o("section")}var f=n(a,[["render",_]]);const d=t({setup(r){return(c,p)=>(e(),s(f))}});export{d as default};
