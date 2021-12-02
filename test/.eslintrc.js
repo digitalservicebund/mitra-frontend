76e60f0b1b0d7fb7782101c21dd092a0d27ccb9b
Klaus Hartl <klaus.hartl@gmail.com>
2021-12-02 19:06:23 +0100

    Introduce testing-library for cypress
    In e2e tests we don't want to select items by CSS classes that may
    change (coupling to styling), but at the same time selecting a visible
    input isn't specific enough and will also break as soon as there are
    more than one input in screen.
    
    Using the testing library addresses both problems with role based
    element selection ability.
    
    These finders are also consistent with our unit testing, where we were
    already using testing-libary.

--- test/.eslintrc.js
+++ test/.eslintrc.js
@@ -1,8 +1,14 @@
 module.exports = {
-  extends: [
-    "../.eslintrc.js",
-    "plugin:jest-dom/recommended",
-    "plugin:testing-library/vue",
-    "plugin:cypress/recommended",
+  extends: ["../.eslintrc.js"],
+  overrides: [
+    {
+      files: ["**/*.ts"],
+      excludedFiles: "e2e/**/*.ts",
+      extends: ["plugin:jest-dom/recommended", "plugin:testing-library/vue"],
+    },
+    {
+      files: ["e2e/**/*.ts"],
+      extends: ["plugin:cypress/recommended"],
+    },
   ],
 }
