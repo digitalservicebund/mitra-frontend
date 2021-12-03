90b19deae88b6e6d2ca369b922bc88c5eb96282c
Shahrooz Afsharipour <shrzaf@gmail.com>
2021-12-03 16:04:35 +0100

    Add missing file from last commit.
    Addresses MIT-96

---
+++ src/infra/CloudContractPlaybook.ts
@@ -0,0 +1,29 @@
+import { TextAnswerStep } from "../domain/Step"
+import Module from "../domain/Module"
+import Playbook from "../domain/Playbook"
+
+const step11 = new TextAnswerStep("Schritt 1.1")
+const step12 = new TextAnswerStep("Schritt 1.2")
+const step13 = new TextAnswerStep("Schritt 1.3")
+
+const step21 = new TextAnswerStep("Schritt 2.1")
+const step22 = new TextAnswerStep("Schritt 2.2")
+const step23 = new TextAnswerStep("Schritt 2.3")
+
+const step31 = new TextAnswerStep("Schritt 3.1")
+const step32 = new TextAnswerStep("Schritt 3.2")
+const step33 = new TextAnswerStep("Schritt 3.3")
+const step34 = new TextAnswerStep("Schritt 3.4")
+
+const module1 = new Module("Rubrum")
+const module2 = new Module("Gegenstand und Bestandteile des Vertrags")
+const module3 = new Module("Gegenstand der Leistungen")
+
+module1.addSteps(step11, step12, step13)
+module2.addSteps(step21, step22, step23)
+module3.addSteps(step31, step32, step33, step34)
+
+const cloudContractPlaybook = new Playbook()
+cloudContractPlaybook.addModules(module1, module2, module3)
+
+export default cloudContractPlaybook
