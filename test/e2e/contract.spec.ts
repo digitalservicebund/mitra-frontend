c76b54d59fb46bd630e550c99e5d3014cf3b12aa
Klaus Hartl <klaus.hartl@gmail.com>
2021-12-06 18:03:00 +0100

    Port first half of E2E tests to Playwright
    Addresses MIT-121

--- test/e2e/contract.spec.ts
+++ test/e2e/contract.spec.ts
@@ -1,46 +1,6 @@
 /// <reference types="cypress" />
 /// <reference types="cypress-file-upload" />
 describe("Starting new contract", () => {
-  it("should be reachable from start page", () => {
-    cy.visit("/")
-    cy.get("main a").contains("Neuer EVB-IT Cloud Vertrag").click()
-    cy.get("main a").should("not.exist") // navigated away
-    cy.contains("Rubrum")
-    cy.contains("Gegenstand der Leistungen")
-  })
-
-  it("should show title dialog", () => {
-    cy.visit("/contract")
-    cy.contains("Vertrag benennen").should("exist")
-  })
-
-  it("should close title dialog on clicking x", () => {
-    cy.visit("/contract")
-    cy.get(".p-dialog-header-close-icon").click()
-    cy.contains("Vertrag benennen").should("not.exist")
-  })
-
-  it("should close title dialog on clicking mask", () => {
-    cy.visit("/contract")
-    // click outside the dialog
-    cy.get("body").click(0, 0)
-    cy.contains("Vertrag benennen").should("not.exist")
-  })
-
-  it("should save edited title on button click", () => {
-    cy.visit("/contract")
-    cy.get("#input-contract-title").type("foo")
-    cy.get("button").contains("OK").click()
-    cy.contains("foo")
-  })
-
-  it("should save edited title on pressing enter", () => {
-    cy.visit("/contract")
-    cy.get("#input-contract-title").type("foo")
-    cy.get("#input-contract-title").type("{enter}")
-    cy.contains("foo")
-  })
-
   it("should list the modules in navigation", () => {
     cy.visit("/contract")
     cy.get("#input-contract-title").type("foo")
--- test/e2e/start-new-contract.spec.ts
+++ test/e2e/start-new-contract.spec.ts
@@ -4,7 +4,40 @@ test("Reachable from start page", async ({ page }) => {
   await page.goto("/mitra-frontend/")
   await page.click("text=Neuer EVB-IT Cloud Vertrag")
   await expect(page).toHaveURL(/\/contract$/)
+})
+
+test("Start with title input dialog", async ({ page }) => {
+  await page.goto("/mitra-frontend/contract")
   await expect(
     page.locator("text=Wie wollen Sie den Vertrag benennen?")
   ).toBeVisible()
+})
+
+test("Close title input dialog on clicking x", async ({ page }) => {
+  await page.goto("/mitra-frontend/contract")
+  await page.click(".p-dialog-header-close-icon")
+  await expect(page.locator("text=Vertrag benennen")).not.toBeVisible()
+})
+
+test("Close title input dialog on clicking mask", async ({ page }) => {
+  await page.goto("/mitra-frontend/contract")
+  // Click outside the dialog..
+  await page.mouse.click(0, 0)
+  await expect(page.locator("text=Vertrag benennen")).not.toBeVisible()
+})
+
+test("should save edited title on button click", async ({ page }) => {
+  await page.goto("/mitra-frontend/contract")
+  await page.fill("#input-contract-title", "foo")
+  await page.click("text=OK")
+  await expect(page.locator("text=Vertrag benennen")).not.toBeVisible()
+  await expect(page.locator("text=foo")).toBeVisible()
+})
+
+test("should save edited title on pressing enter", async ({ page }) => {
+  await page.goto("/mitra-frontend/contract")
+  await page.fill("#input-contract-title", "foo")
+  await page.press("#input-contract-title", "Enter")
+  await expect(page.locator("text=Vertrag benennen")).not.toBeVisible()
+  await expect(page.locator("text=foo")).toBeVisible()
 })
