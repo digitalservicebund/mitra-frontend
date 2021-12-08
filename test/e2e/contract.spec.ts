ea9be2dfb8bbe214b2a38c147d7c86a439fa2444
Klaus Hartl <klaus.hartl@gmail.com>
2021-12-08 17:35:41 +0100

    Port last remaining e2e test to Playwright
    Drag and dropping a Playbook file: note that the test is cheating, but
    not cheating more than what was done in cypress-file-upload, where, to
    emulate a drag and drop operation, the file input `files` property is
    being altered:
    
    https://github.com/abramenal/cypress-file-upload/blob/2cb3cb13512eef9f034a2c226ec5845d9d76f244/src/helpers/attachFileToElement.ts#L69
    
    With Playwright the approach is very much the same for now,
    looking up the drop zone, followed by adapting the input file state.
    
    Addresses MIT-121

--- test/e2e/contract.spec.ts
+++ test/e2e/contract.spec.ts
@@ -1,13 +1,5 @@
 /// <reference types="cypress" />
 /// <reference types="cypress-file-upload" />
 describe("Starting new contract", () => {
-  it("should open playbook from filesystem by dragging into drop zone", () => {
-    cy.visit("/open-playbook")
-    cy.get(".p-fileupload-content").attachFile("contract.json", {
-      subjectType: "drag-n-drop",
-    })
-    cy.contains("Vertrag benennen").should("exist")
-    cy.get(".p-dialog-header-close-icon").click()
-    cy.get("body").should("contain.text", "foo module")
-  })
+  it("should open playbook from filesystem by dragging into drop zone")
 })
--- test/e2e/open-playbook.spec.ts
+++ test/e2e/open-playbook.spec.ts
@@ -13,6 +13,22 @@ test.describe("Open playbook from filesystem", async () => {
     const { getByText } = queries
     await getByText(document, "foo module")
   })
+
+  test("via drag and drop", async ({ page, baseURL }) => {
+    await page.goto(`${baseURL}/mitra-frontend/open-playbook`)
+    await page
+      .dragAndDrop("html", ".p-fileupload-content")
+      .then(() =>
+        page.setInputFiles(
+          "input[type=file]",
+          "./test/e2e/fixtures/playbook.json"
+        )
+      )
+    await page.click(".p-dialog-header-close-icon")
+    const document = await getDocument(page)
+    const { getByText } = queries
+    await getByText(document, "foo module")
+  })
 })
 
 test("Reload cloud contract after having created contract from loaded playbook", async ({
