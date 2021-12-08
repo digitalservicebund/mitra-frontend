014889b9ba0540d281a284ce577554da6d8785bc
Klaus Hartl <klaus.hartl@gmail.com>
2021-12-08 14:26:15 +0100

    Port further, file upload related e2e tests
    Addresses MIT-121

--- test/e2e/contract.spec.ts
+++ test/e2e/contract.spec.ts
@@ -1,14 +1,6 @@
 /// <reference types="cypress" />
 /// <reference types="cypress-file-upload" />
 describe("Starting new contract", () => {
-  it("should open playbook from filesystem", () => {
-    cy.visit("/open-playbook")
-    cy.get("input[type='file']").attachFile("contract.json")
-    cy.contains("Vertrag benennen").should("exist")
-    cy.get(".p-dialog-header-close-icon").click()
-    cy.get("body").should("contain.text", "foo module")
-  })
-
   it("should open playbook from filesystem by dragging into drop zone", () => {
     cy.visit("/open-playbook")
     cy.get(".p-fileupload-content").attachFile("contract.json", {
@@ -17,18 +9,5 @@ describe("Starting new contract", () => {
     cy.contains("Vertrag benennen").should("exist")
     cy.get(".p-dialog-header-close-icon").click()
     cy.get("body").should("contain.text", "foo module")
-  })
-
-  it("should reload cloud contract when navigating from start page after having loaded contract from filesystem", () => {
-    cy.visit("/open-playbook")
-    cy.get(".p-fileupload-content").attachFile("contract.json", {
-      subjectType: "drag-n-drop",
-    })
-    cy.contains("Vertrag benennen").should("exist")
-    cy.get("body").should("contain.text", "foo module")
-    cy.get(".p-dialog-header-close-icon").click()
-    cy.get("a").contains("Startseite").click()
-    cy.get("main a").contains("Neuer EVB-IT Cloud Vertrag").click()
-    cy.get("body").should("contain.text", "Rubrum")
   })
 })
--- test/e2e/start-new-contract.spec.ts
+++ test/e2e/start-new-contract.spec.ts
@@ -1,4 +1,4 @@
-import { test, expect } from "@playwright/test"
+import { expect, test } from "@playwright/test"
 import { getDocument, queries } from "@playwright-testing-library/test"
 
 test("Reachable from start page", async ({ page, baseURL }) => {
