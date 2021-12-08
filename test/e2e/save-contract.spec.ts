d637a8b190ae41df00a0dcff29e8f1da013d4f2e
Klaus Hartl <klaus.hartl@gmail.com>
2021-12-08 09:43:37 +0100

    Skip test the proper way
    Addresses MIT-121

--- test/e2e/save-contract.spec.ts
+++ test/e2e/save-contract.spec.ts
@@ -1,31 +1,30 @@
 import * as fs from "fs"
-import { test, expect } from "@playwright/test"
+import { expect, test } from "@playwright/test"
 
-test("Save currently worked on contract to disk", async ({
-  page,
-  browserName,
-}) => {
-  // Chrome chrashes upon invoking `window.showSaveFilePicker`...!
-  if (browserName !== "chromium") {
-    await page.goto("/mitra-frontend/contract/cloud-contract")
-    await page.locator("text=Vertrag benennen").waitFor()
-    await page.fill("#input-contract-title", "TEST")
-    await page.click("text=OK")
+test.skip(
+  ({ browserName }) => browserName === "chromium",
+  "Chrome chrashes upon invoking `window.showSaveFilePicker`... => https://github.com/microsoft/playwright/issues/7339"
+)
 
-    page.on("download", (download) => {
-      download.createReadStream().then((stream) => {
-        const writable = fs.createWriteStream("./test/e2e/saved-contract.json")
-        stream.pipe(writable)
+test("Save currently worked on contract to disk", async ({ page }) => {
+  await page.goto("/mitra-frontend/contract/cloud-contract")
+  await page.locator("text=Vertrag benennen").waitFor()
+  await page.fill("#input-contract-title", "TEST")
+  await page.click("text=OK")
 
-        const downloaded = fs.readFileSync(
-          "./test/e2e/saved-contract.json",
-          "utf-8"
-        )
+  page.on("download", (download) => {
+    download.createReadStream().then((stream) => {
+      const writable = fs.createWriteStream("./test/e2e/saved-contract.json")
+      stream.pipe(writable)
 
-        expect(() => JSON.parse(downloaded)).not.toThrow()
-        expect(JSON.parse(downloaded)).toMatchObject({ title: "TEST" })
-      })
+      const downloaded = fs.readFileSync(
+        "./test/e2e/saved-contract.json",
+        "utf-8"
+      )
+
+      expect(() => JSON.parse(downloaded)).not.toThrow()
+      expect(JSON.parse(downloaded)).toMatchObject({ title: "TEST" })
     })
-    await page.click("text=Speichern")
-  }
+  })
+  await page.click("text=Speichern")
 })
