8a7d84ceea5e32a0c2204cdad4fdd675ae45ccbd
Klaus Hartl <klaus.hartl@gmail.com>
2021-12-10 17:35:23 +0100

    Use material icon for back button
    Just like start screen..

--- src/components/NavigateToHome.vue
+++ src/components/NavigateToHome.vue
@@ -1,5 +1,10 @@
 <template>
   <router-link to="/mitra-frontend/" class="block mb-4">
-    <span class="pi pi-angle-left text-lg">Startseite</span>
+    <span
+      class="material-icons-outlined text-lg align-bottom mr-1"
+      aria-hidden="true"
+      >arrow_back</span
+    >
+    <span class="text-lg">Startseite</span>
   </router-link>
 </template>
