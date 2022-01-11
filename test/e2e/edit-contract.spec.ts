e327ce9633339d68e826c1c535e158b7550a85e8
Klaus Hartl <klaus.hartl@gmail.com>
2022-01-11 18:26:39 +0100

    Streamline E2E step editing tests
    In order to avoid testing certain things over and over (navigating
    through a contract to reach a certain step) we start utilizing the
    Playwright capability to set up localStorage with a given state, in this
    case contracts with particular steps we'd like to test. This won't scale
    when testing new step types while relying on a single contract to test
    with. New tests would need more navigation.
    
    Furthermore the adapted E2E tests are now decoupled from the (evolving)
    contract in the data directory, thus are less likely to break when data
    changes, i.e. will require less maintenance.
    
    This is going to make tests faster and more focussed on the subject
    under test.

--- test/e2e/edit-contract.spec.ts
+++ test/e2e/edit-contract.spec.ts
@@ -1,49 +1,49 @@
 import { getDocument, queries } from "@playwright-testing-library/test"
-import { expect, Page, test as base } from "@playwright/test"
+import { expect, test } from "@playwright/test"
 
 const { findByLabelText, findByText, findByTitle } = queries
 
-// Set up fixture..
-const test = base.extend<{ page: Page }>({
-  page: async ({ page, baseURL }, use) => {
-    await page.goto(`${baseURL}/mitra-frontend/new-contract`)
-    await page.locator("text=Vertrag benennen").waitFor()
-    await page.mouse.click(0, 0) // Dismiss dialog..
-    await use(page)
-  },
+test.describe("Text answer step", async () => {
+  test.use({
+    storageState: "./test/e2e/fixtures/contract-text-answer-step-state.json",
+  })
+  test("editing unanswered", async ({ page, baseURL }) => {
+    await page.goto(
+      `${baseURL}/mitra-frontend/contract/3d324eca-06c2-4781-af52-705f49039d0d`
+    )
+    const document = await getDocument(page)
+    await findByTitle(document, "Test step").then((input) =>
+      input.type("Answer to show up in preview")
+    )
+    const preview = await page.locator("section:right-of(main)").elementHandle()
+    await findByText(preview, "Answer to show up in preview")
+  })
 })
 
-test("Edit text answer", async ({ page }) => {
-  const document = await getDocument(page)
-  await findByTitle(document, "Schritt 1.1").then((input) =>
-    input.type("Answer to show up in preview")
-  )
-  const preview = await page.locator("section:right-of(main)").elementHandle()
-  await findByText(preview, "Vorschau Vertragstext")
-  await findByText(preview, "Rubrum")
-  await findByText(preview, "Textbaustein: Answer to show up in preview")
-})
-
-test("Edit single choice answer", async ({ page }) => {
-  const document = await getDocument(page)
-  await page.locator("text=Schritt 1.1").waitFor()
-  await page.click("text=Weiter")
-  await page.locator("text=Schritt 1.2").waitFor()
-  await page.click("text=Weiter")
-  await findByLabelText(document, "Auswahl B")
-  await page.click("text=Auswahl B")
-  // NOTE: tried `findByRole(document, "radio")` at first but didn't work...
-  await expect(
-    page.locator("li:has(label:text('Auswahl B')) >> [role=radio]")
-  ).toBeChecked()
-  const preview = await page.locator("section:right-of(main)").elementHandle()
-  await findByText(preview, "Vorschau Vertragstext")
-  await findByText(preview, "Auswahl B")
-  await page.click("text=Weiter")
-  await findByTitle(document, "Schritt 1.3 B").then((input) =>
-    input.type("Answer to show up in preview")
-  )
-  await findByText(preview, "Answer to show up in preview")
-  await page.click("text=Weiter")
-  await findByTitle(document, "Schritt 2.1")
+test.describe("Single choice answer step", async () => {
+  test.use({
+    storageState: "./test/e2e/fixtures/contract-single-choice-step-state.json",
+  })
+  test("editing unanswered", async ({ page, baseURL }) => {
+    await page.goto(
+      `${baseURL}/mitra-frontend/contract/3d324eca-06c2-4781-af52-705f49039d0d`
+    )
+    const document = await getDocument(page)
+    await findByLabelText(document, "Test choice 1A")
+    await findByLabelText(document, "Test choice 1B")
+    await page.click("text=Test choice 1B")
+    // NOTE: tried `findByRole(document, "radio")` at first but didn't work...
+    await expect(
+      page.locator("li:has(label:text('Test choice 1B')) >> [role=radio]")
+    ).toBeChecked()
+    const preview = await page.locator("section:right-of(main)").elementHandle()
+    await findByText(preview, "Test choice 1B")
+    await page.click("text=Weiter")
+    await findByTitle(document, "Test step 1B").then((input) =>
+      input.type("Answer to show up in preview")
+    )
+    await findByText(preview, "Answer to show up in preview")
+    await page.click("text=Weiter")
+    await findByTitle(document, "Test step 2")
+  })
 })
