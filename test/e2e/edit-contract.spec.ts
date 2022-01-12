b6aaef5dfd7cc28cffc2485dc1935d27c7c4602e
Klaus Hartl <klaus.hartl@gmail.com>
2022-01-12 13:39:34 +0100

    Reintroduce fixture, avoid test setup duplication

--- test/e2e/edit-contract.spec.ts
+++ test/e2e/edit-contract.spec.ts
@@ -1,31 +1,41 @@
 import fs from "fs"
 import { getDocument, queries } from "@playwright-testing-library/test"
-import { expect, test } from "@playwright/test"
+import { expect, test as base } from "@playwright/test"
 
 const { findByLabelText, findByText, findByTitle } = queries
 
+type TestFixtures = {
+  contractFile: string
+  contract: string
+}
+
+const test = base.extend<TestFixtures>({
+  contractFile: "", // needs to be filled in per test.use(...) atm, see below
+  contract: async ({ contractFile }, use) => {
+    const contract = await fs.promises.readFile(contractFile, "utf-8")
+    await use(contract)
+  },
+  storageState: async ({ contract }, use) => {
+    await use({
+      cookies: [],
+      origins: [
+        {
+          origin: "http://localhost:5000",
+          localStorage: [
+            {
+              name: "3d324eca-06c2-4781-af52-705f49039d0d",
+              value: `${contract}`,
+            },
+          ],
+        },
+      ],
+    })
+  },
+})
+
 test.describe("Text answer step", async () => {
   test.use({
-    storageState: async ({}, use) => {
-      const contract = await fs.promises.readFile(
-        "./test/e2e/fixtures/contract-text-answer-step.json",
-        "utf-8"
-      )
-      await use({
-        cookies: [],
-        origins: [
-          {
-            origin: "http://localhost:5000",
-            localStorage: [
-              {
-                name: "3d324eca-06c2-4781-af52-705f49039d0d",
-                value: `${contract}`,
-              },
-            ],
-          },
-        ],
-      })
-    },
+    contractFile: "./test/e2e/fixtures/contract-text-answer-step.json",
   })
 
   test("editing unanswered", async ({ page, baseURL }) => {
@@ -43,26 +53,7 @@ test.describe("Text answer step", async () => {
 
 test.describe("Single choice answer step", async () => {
   test.use({
-    storageState: async ({}, use) => {
-      const contract = await fs.promises.readFile(
-        "./test/e2e/fixtures/contract-single-choice-step.json",
-        "utf-8"
-      )
-      await use({
-        cookies: [],
-        origins: [
-          {
-            origin: "http://localhost:5000",
-            localStorage: [
-              {
-                name: "3d324eca-06c2-4781-af52-705f49039d0d",
-                value: `${contract}`,
-              },
-            ],
-          },
-        ],
-      })
-    },
+    contractFile: "./test/e2e/fixtures/contract-single-choice-step.json",
   })
 
   test("editing unanswered", async ({ page, baseURL }) => {
