77688945c6a5eda1cf401e1cc18619fbd737943d
carhartl <carhartl@users.noreply.github.com>
2022-01-13 12:54:50 +0000

    deploy: b113a66603d05fc10fb85bafafd06aa15875a3ab

--- assets/new-contract.856c5167.js
+++
@@ -1,1 +0,0 @@
-import{i as a,m as r,n,o as s,j as c}from"./vendor.f7297514.js";import{C as i,m,a as p}from"./provide.c61aed06.js";import"./index.81ae2979.js";const k=a({setup(d){const t=p(),o=i.fromPlaybook(m().findById("db2a1d38-01fb-4ea2-bc6f-b5213413c809"));return t.save(o),r(async()=>{await n().push(`/mitra-frontend/contract/${o.id}`)}),(e,f)=>(s(),c("div"))}});export{k as default};
