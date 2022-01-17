9eae24669c19d9affc0a4a7dc279f9d97ddd1f38
Klaus Hartl <klaus.hartl@gmail.com>
2022-01-17 18:16:22 +0100

    Add text und list editing buttons to editor
    Addresses MIT-95

--- src/components/RichTextAnswerPreview.vue
+++ src/components/RichTextAnswerPreview.vue
@@ -1,5 +1,12 @@
 <script setup lang="ts">
-  import StarterKit from "@tiptap/starter-kit"
+  import BoldExtension from "@tiptap/extension-bold"
+  import DocumentExtension from "@tiptap/extension-document"
+  import ItalicExtension from "@tiptap/extension-italic"
+  import ListItemExtension from "@tiptap/extension-list-item"
+  import OrderedListExtension from "@tiptap/extension-ordered-list"
+  import ParagraphExtension from "@tiptap/extension-paragraph"
+  import TextExtension from "@tiptap/extension-text"
+  import UnderlineExtension from "@tiptap/extension-underline"
   import { useEditor, EditorContent } from "@tiptap/vue-3"
   import { onBeforeUnmount, watch } from "vue"
   import { RichTextAnswerStep } from "../domain/Step"
@@ -7,7 +14,16 @@
   const props = defineProps<{ step: RichTextAnswerStep }>()
 
   const editor = useEditor({
-    extensions: [StarterKit],
+    extensions: [
+      BoldExtension,
+      DocumentExtension,
+      ItalicExtension,
+      ListItemExtension,
+      OrderedListExtension,
+      ParagraphExtension,
+      TextExtension,
+      UnderlineExtension,
+    ],
     editable: false,
     content: props.step.print(),
   })
--- src/components/RichTextAnswerStep.vue
+++ src/components/RichTextAnswerStep.vue
@@ -1,5 +1,12 @@
 <script setup lang="ts">
-  import StarterKit from "@tiptap/starter-kit"
+  import BoldExtension from "@tiptap/extension-bold"
+  import DocumentExtension from "@tiptap/extension-document"
+  import ItalicExtension from "@tiptap/extension-italic"
+  import ListItemExtension from "@tiptap/extension-list-item"
+  import OrderedListExtension from "@tiptap/extension-ordered-list"
+  import ParagraphExtension from "@tiptap/extension-paragraph"
+  import TextExtension from "@tiptap/extension-text"
+  import UnderlineExtension from "@tiptap/extension-underline"
   import { useEditor, EditorContent } from "@tiptap/vue-3"
   import { onBeforeUnmount } from "vue"
   import { RichTextAnswerStep } from "../domain/Step"
@@ -7,7 +14,16 @@
   const props = defineProps<{ step: RichTextAnswerStep }>()
 
   const editor = useEditor({
-    extensions: [StarterKit],
+    extensions: [
+      BoldExtension,
+      DocumentExtension,
+      ItalicExtension,
+      ListItemExtension,
+      OrderedListExtension,
+      ParagraphExtension,
+      TextExtension,
+      UnderlineExtension,
+    ],
     content: props.step.print(),
     onUpdate: ({ editor }) => props.step.answer.setValue(editor.getHTML()),
     editorProps: {
@@ -25,9 +41,53 @@
     {{ step.text }}
   </div>
   <div class="mb-4">
+    <div v-if="editor">
+      <button
+        :class="{
+          'font-bold': editor.isActive('bold'),
+          border: true,
+          'p-2': true,
+        }"
+        @click="editor?.chain().focus().toggleBold().run()"
+      >
+        bold
+      </button>
+      <button
+        :class="{
+          'font-bold': editor.isActive('italic'),
+          border: true,
+          'p-2': true,
+        }"
+        @click="editor?.chain().focus().toggleItalic().run()"
+      >
+        italic
+      </button>
+      <button
+        :class="{
+          'font-bold': editor.isActive('underline'),
+          border: true,
+          'p-2': true,
+        }"
+        @click="editor?.chain().focus().toggleUnderline().run()"
+      >
+        underline
+      </button>
+      <button
+        :class="{
+          'font-bold': editor.isActive('orderedList'),
+          border: true,
+          'p-2': true,
+        }"
+        @click="editor?.chain().focus().toggleOrderedList().run()"
+      >
+        list
+      </button>
+    </div>
     <editor-content
       :editor="editor"
       class="border rounded border-neutral-300"
     />
   </div>
 </template>
+
+<style></style>
