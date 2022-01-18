bb8d1a3d589643f02d5b377c490bc57a7dfad629
Klaus Hartl <klaus.hartl@gmail.com>
2022-01-18 09:27:01 +0100

    Extract shared rich text editor config
    Addresses MIT-95

--- src/components/RichTextAnswerStep.vue
+++ src/components/RichTextAnswerStep.vue
@@ -1,29 +1,13 @@
 <script setup lang="ts">
-  import BoldExtension from "@tiptap/extension-bold"
-  import DocumentExtension from "@tiptap/extension-document"
-  import ItalicExtension from "@tiptap/extension-italic"
-  import ListItemExtension from "@tiptap/extension-list-item"
-  import OrderedListExtension from "@tiptap/extension-ordered-list"
-  import ParagraphExtension from "@tiptap/extension-paragraph"
-  import TextExtension from "@tiptap/extension-text"
-  import UnderlineExtension from "@tiptap/extension-underline"
   import { useEditor, EditorContent } from "@tiptap/vue-3"
   import { onBeforeUnmount } from "vue"
   import { RichTextAnswerStep } from "../domain/Step"
+  import RichTextEditorConfig from "./RichTextEditorConfig"
 
   const props = defineProps<{ step: RichTextAnswerStep }>()
 
   const editor = useEditor({
-    extensions: [
-      BoldExtension,
-      DocumentExtension,
-      ItalicExtension,
-      ListItemExtension,
-      OrderedListExtension,
-      ParagraphExtension,
-      TextExtension,
-      UnderlineExtension,
-    ],
+    ...RichTextEditorConfig,
     content: props.step.print(),
     onUpdate: ({ editor }) => props.step.answer.setValue(editor.getHTML()),
     editorProps: {
---
+++ src/components/RichTextEditorConfig.ts
@@ -0,0 +1,21 @@
+import BoldExtension from "@tiptap/extension-bold"
+import DocumentExtension from "@tiptap/extension-document"
+import ItalicExtension from "@tiptap/extension-italic"
+import ListItemExtension from "@tiptap/extension-list-item"
+import OrderedListExtension from "@tiptap/extension-ordered-list"
+import ParagraphExtension from "@tiptap/extension-paragraph"
+import TextExtension from "@tiptap/extension-text"
+import UnderlineExtension from "@tiptap/extension-underline"
+
+export default {
+  extensions: [
+    BoldExtension,
+    DocumentExtension,
+    ItalicExtension,
+    ListItemExtension,
+    OrderedListExtension,
+    ParagraphExtension,
+    TextExtension,
+    UnderlineExtension,
+  ],
+}
