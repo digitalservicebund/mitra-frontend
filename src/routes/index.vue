2730d921b3abf2fdf4606660b009a0a2dc2a9d46
Klaus Hartl <klaus.hartl@gmail.com>
2022-01-20 17:43:47 +0100

    Remove redundant prefix from component name
    The directory they're in denotes...

--- src/routes/index.vue
+++ src/routes/index.vue
@@ -1,6 +1,6 @@
 <!-- Pages are solely responsible for providing a route -->
 <script setup lang="ts">
-  import Screen from "../components/creator/CreatorStartScreen.vue"
+  import Screen from "../components/creator/StartScreen.vue"
 </script>
 
 <template>
