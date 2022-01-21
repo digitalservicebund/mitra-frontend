26e032bee2ef857ff12d0b4beffc6a22154701a2
Florian <florian.zechmeister@digitalservice.bund.de>
2022-01-21 12:35:26 +0100

    Adjust integration tests for new urls
    Addresses MIT-149

--- test/e2e/creator/start-new-contract.spec.ts
+++ test/e2e/creator/start-new-contract.spec.ts
@@ -7,9 +7,9 @@ test("Reachable from creator's start page", async ({ page, baseURL }) => {
 
   await page.goto(`${baseURL}/mitra-frontend/creator`)
   await page.click("text=Vertrag weiter bearbeiten")
-  await expect(page).toHaveURL(/.*open-contract/)
+  await expect(page).toHaveURL(/contract\/open/)
 
   await page.goto(`${baseURL}/mitra-frontend/creator`)
   await page.click("text=Neuer Vertrag aus lokalem Playbook")
-  await expect(page).toHaveURL(/.*contract\/open-playbook/)
+  await expect(page).toHaveURL(/contract\/open-playbook/)
 })
