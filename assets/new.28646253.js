4d95b47624bbf2f3709bdad0f486b74a05706077
carhartl <carhartl@users.noreply.github.com>
2022-01-25 11:40:54 +0000

    deploy: 359600a56bdb66a89a3a6000012eeb54a4cd6fd2

---
+++ assets/new.28646253.js
@@ -0,0 +1,1 @@
+import{f as t,a0 as r,a1 as s,o as n,g as p}from"./vendor.4f77a301.js";import{P as c,c as i}from"./provide.7f773723.js";import"./index.ed5b3cd0.js";const k=t({setup(d){const e=i(),o=new c;return e.save(o),r(async()=>{await s().push(`/mitra-frontend/playbook/${o.id}`)}),(a,f)=>(n(),p("div"))}});export{k as default};
--- assets/new.850c08fe.js
+++
@@ -1,1 +0,0 @@
-import{f as t,a0 as r,a1 as s,o as n,g as c}from"./vendor.4f77a301.js";import{P as p,c as i}from"./provide.07896cc7.js";import"./index.bba3cde9.js";const k=t({setup(m){const e=i(),o=new p;return e.save(o),r(async()=>{await s().push(`/mitra-frontend/playbook/${o.id}`)}),(a,u)=>(n(),c("div"))}});export{k as default};
