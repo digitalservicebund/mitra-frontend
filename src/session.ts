80ee0e329f3448836adc366c2e1d6aad8fe2dfa5
Florian <florian.zechmeister@digitalservice.bund.de>
2022-01-25 20:22:55 +0100

    Add `entryPoint` to session
    In order to navigate buyers and creators to their respective start
    screen we need to remember the entry point.
    
    Addresses MIT-149

--- src/session.ts
+++ src/session.ts
@@ -1,13 +1,19 @@
 import { defineStore } from "pinia"
 import { Answer, Step } from "./domain/Step"
 
+export type EntryPoint = "creator" | "buyer"
+
 export const useSession = defineStore("session", {
   state: () => ({
     cache: new Map<string, Step<Answer>>(),
+    entryPoint: "",
   }),
   actions: {
     rememberCurrentStep({ id }: { id: string }, step: Step<Answer>) {
       this.cache.set(id, step)
+    },
+    rememberEntryPoint(entryPoint: EntryPoint) {
+      this.entryPoint = entryPoint
     },
   },
 })
