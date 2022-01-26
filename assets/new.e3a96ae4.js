7913082eb6f9c65e314578e31fcdc8920f22ce67
carhartl <carhartl@users.noreply.github.com>
2022-01-26 11:23:05 +0000

    deploy: 6a7c829567b7317d0519e92d4a8e2d7328b70e2a

---
+++ assets/new.e3a96ae4.js
@@ -0,0 +1,1 @@
+import{f as t,a0 as r,a1 as s,o as n,g as p}from"./vendor.d1ed8eac.js";import{P as c,c as i}from"./provide.9ffd6161.js";import"./main.f323bbcb.js";const b=t({setup(f){const e=i(),o=new c;return e.save(o),r(async()=>{await s().push(`/mitra-frontend/playbook/${o.id}`)}),(a,m)=>(n(),p("div"))}});export{b as default};
