b2f1525bb7975a07ed63420fcf0ada4b6ac6a6c6
Klaus Hartl <klaus.hartl@gmail.com>
2022-01-28 19:49:26 +0100

    Add styling for playbook/contract metadata
    Addresses MIT-150

--- src/components/Metadata.vue
+++ src/components/Metadata.vue
@@ -10,15 +10,15 @@
 </script>
 
 <template>
-  <dl>
-    <dt>Datei erstellt am</dt>
+  <dl class="flex flex-col flex-wrap max-h-12 mt-4">
+    <dt class="text-xs">Datei erstellt am</dt>
     <dd>
       <time v-if="createdAt" :datetime="createdAt.toISOString()">{{
         createdAt.toLocaleDateString(locale, options)
       }}</time>
       <span v-else>-</span>
     </dd>
-    <dt>Zuletzt gespeichert am</dt>
+    <dt class="text-xs">Zuletzt gespeichert am</dt>
     <dd>
       <time v-if="savedAt" :datetime="savedAt.toISOString()">{{
         savedAt.toLocaleDateString(locale, options)
