e2f62ca5368376d0006586c6d65e4b0a9dfc9fc9
zechmeister <zechmeister@users.noreply.github.com>
2022-02-01 23:30:06 +0000

    deploy: d6ffb47b7c37ce8da309b1c41b768c0e3d8fb73b

--- assets/new.3d75a19d.js
+++
@@ -1,1 +0,0 @@
-import{f as t,a5 as r,a6 as s,o as n,g as p}from"./vendor.301f9710.js";import{P as c,c as i}from"./provide.1a5456d4.js";import"./main.930667e4.js";const k=t({setup(m){const e=i(),o=new c;return e.save(o),r(async()=>{await s().push(`/mitra-frontend/playbook/${o.id}`)}),(a,u)=>(n(),p("div"))}});export{k as default};
