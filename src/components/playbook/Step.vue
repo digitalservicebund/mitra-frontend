458e4da5c3aa1ac5186e093434efa4e85e9875ea
Florian <florian.zechmeister@digitalservice.bund.de>
2022-02-04 18:50:56 +0100

    Implement basic context menu
    Addresses MIT-166

--- src/components/playbook/Step.vue
+++ src/components/playbook/Step.vue
@@ -1,13 +1,50 @@
 <script setup lang="ts">
+  import Button from "primevue/button"
+  import ContextMenu from "primevue/contextmenu"
+  import { ref } from "vue"
   import { Answer } from "../../domain/Answer"
   import { Step } from "../../domain/Step"
 
   defineProps<{ step: Step<Answer> }>()
+
+  const menu = ref()
+  const items = [
+    {
+      label: "Neue Frage",
+    },
+    {
+      label: "Frage duplizieren",
+    },
+    {
+      label: "Frage löschen",
+    },
+  ]
+
+  const openMenu = (event: Event) => {
+    console.log(menu.value)
+    menu.value.show(event)
+  }
 </script>
 
 <template>
   <details open>
-    <summary class="text-lg">{{ step.text }}</summary>
+    <summary class="text-lg relative">
+      {{ step.text }}
+      <Button
+        type="button"
+        class="absolute top-0 right-0"
+        aria-label="menu"
+        aria-haspopup="menu"
+        @contextmenu="openMenu"
+        @click="openMenu"
+        @keypress.m="openMenu"
+      >
+        <span class="material-icons-outlined text-base" aria-hidden="true">
+          more_vert
+        </span>
+      </Button>
+      <ContextMenu ref="menu" :model="items" />
+    </summary>
     <p>Typ: {{ step.type }}</p>
   </details>
 </template>
---
+++ test/components/playbook/Step.spec.ts
@@ -0,0 +1,26 @@
+import { render, screen } from "@testing-library/vue"
+import Step from "../../../src/components/playbook/Step.vue"
+import { TextAnswerStep } from "../../../src/domain/Step"
+
+describe("Step", () => {
+  const step = new TextAnswerStep("Fragetext eintragen")
+
+  it("displays the title", async () => {
+    render(Step, {
+      props: {
+        step: step,
+      },
+    })
+    await screen.findByText("Fragetext eintragen")
+  })
+
+  it("has a menu button", async () => {
+    render(Step, {
+      props: {
+        step: step,
+      },
+    })
+
+    await screen.findByLabelText("menu")
+  })
+})
