4ef79c0f179748ae5c505d359548659a9f2c165b
zechmeister <zechmeister@users.noreply.github.com>
2022-02-07 21:05:53 +0000

    deploy: 4a074c94c5833fc97eeba492f31bbf3789fa0016

--- assets/new.9317a7f5.js
+++
@@ -1,1 +0,0 @@
-import{f as e,a5 as r,a6 as s,o as n,g as c}from"./vendor.301f9710.js";import{C as f,c as i,m}from"./provide.1b9ef453.js";import"./main.d37d4abf.js";const k=e({setup(p){const t=m(),o=f.fromPlaybook(i().findById("db2a1d38-01fb-4ea2-bc6f-b5213413c809"));return t.save(o),r(async()=>{await s().push(`/mitra-frontend/contract/${o.id}`)}),(a,d)=>(n(),c("div"))}});export{k as default};
