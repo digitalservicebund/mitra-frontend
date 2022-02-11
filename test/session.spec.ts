a212ac183c7b91875af6fbac855dc78dadfccc6f
Klaus Hartl <klaus.hartl@gmail.com>
2022-02-11 19:33:06 +0100

    Add unit test for hydrating session
    Addresses MIT-204

--- test/session.spec.ts
+++ test/session.spec.ts
@@ -1,14 +1,21 @@
-import { createTestingPinia } from "@pinia/testing"
+import { setActivePinia, createPinia } from "pinia"
+import piniaPersist from "pinia-plugin-persist"
+import { createApp } from "vue"
 import Contract from "../src/domain/Contract"
 import Module from "../src/domain/Module"
 import { TextAnswerStep } from "../src/domain/Step"
 import { useSession } from "../src/session"
 
 describe("session", () => {
-  createTestingPinia()
-  const session = useSession()
+  beforeAll(() => {
+    const app = createApp({})
+    const pinia = createPinia().use(piniaPersist)
+    app.use(pinia)
+    setActivePinia(pinia)
+  })
 
   it("remembers contract without last edited step", () => {
+    const session = useSession()
     const step = new TextAnswerStep("remembered")
     const contract = new Contract("foo", [new Module("bar", [step])])
 
@@ -19,6 +26,7 @@ describe("session", () => {
   })
 
   it("remembers contract along with last edited step", () => {
+    const session = useSession()
     const step = new TextAnswerStep("remembered")
     const contract = new Contract("foo", [
       new Module("bar", [new TextAnswerStep("baz"), step]),
@@ -28,5 +36,14 @@ describe("session", () => {
 
     expect(session.contract).toStrictEqual(contract)
     expect(session.lastEditedStep).toStrictEqual(step)
+  })
+
+  it("hydrates contract instance from serialized sessionStorage", () => {
+    sessionStorage.setItem(
+      "session",
+      '{"cache":[{"id":"3d324eca-06c2-4781-af52-705f49039d0d","title":"test contract","modules":[{"id":"930fe484-bd57-481d-8846-2d1a8b3e35db","title":"foo module","steps":[{"id":"67d574b0-2b77-42a5-bcd2-5ed600849adb","prompt":"bar","type":"TextAnswerStep","answer":{}}]}]},"c2623964-fb74-44ee-9384-b24e6decf11e",{"createdAt":"2022-02-11T18:16:35.691Z"}]}'
+    )
+    const session = useSession()
+    expect(session.contract).toBeInstanceOf(Contract)
   })
 })
