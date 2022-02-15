dca725d20b3200482280efda41bbe95fdb35009c
zechmeister <zechmeister@users.noreply.github.com>
2022-02-15 11:18:49 +0000

    deploy: 5eda58206cebd52c80bcc7c6cf468b09e8000ecc

--- assets/creator.2ecb402d.js
+++
@@ -1,1 +0,0 @@
-import{_ as o}from"./StartScreenCreator.edc95302.js";import{f as r,o as t,c as e}from"./vendor.4eaa413b.js";import"./CreateContractPanel.4107d8e0.js";import"./session.6dfcfa62.js";const f=r({setup(a){return(c,p)=>(t(),e(o))}});export{f as default};
---
+++ assets/new.0432ef46.js
@@ -0,0 +1,1 @@
+import{f as a,a7 as s,a8 as n,o as t,g as r}from"./vendor.4eaa413b.js";import{u as c,P as p}from"./session.6b86ee47.js";const k=a({setup(i){const e=c(),o=new p;return e.rememberPlaybook(o),s(async()=>await n().replace(`/mitra-frontend/playbook/${o.id}`)),(m,l)=>(t(),r("div"))}});export{k as default};
