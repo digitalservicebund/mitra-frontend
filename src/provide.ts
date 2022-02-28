c82ec790ea402d044c33f260bdd5d6347c7613d6
Klaus Hartl <klaus.hartl@gmail.com>
2022-02-28 11:36:51 +0100

    Switch back to in-memory playbook repository
    With recent changes to session we can avoid localStorage.

--- src/provide.ts
+++ src/provide.ts
@@ -1,7 +1,7 @@
 import ContractStorageService from "./domain/ContractStorageService"
 import PlaybookStorageService from "./domain/PlaybookStorageService"
 import contractFileStorage from "./infra/ContractFileStorage"
-import playbookRepository from "./infra/LocalStoragePlaybookRepository"
+import playbookRepository from "./infra/InMemoryPlaybookRepository"
 import playbookFileStorage from "./infra/PlaybookFileStorage"
 
 export const makePlaybookRepository = () => playbookRepository
