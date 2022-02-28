58b9501942fed047b364bdc6646e2c298a9031e6
Klaus Hartl <klaus.hartl@gmail.com>
2022-02-28 11:26:58 +0100

    Revert "Remove no longer used repository implementation"
    This reverts commit 491aec291032e38023c1d68f2f3561fc9ff68113.

---
+++ src/infra/InMemoryPlaybookRepository.ts
@@ -0,0 +1,24 @@
+import { playbook } from "../data/evb-it-cloud-playbook.json"
+import Playbook from "../domain/Playbook"
+import PlaybookRepository from "../domain/PlaybookRepository"
+import { createPlaybook, PlaybookDTO } from "./JSONMapper"
+
+const data = new Map<string, Playbook>()
+
+const cloudContractPlaybook = createPlaybook(playbook as PlaybookDTO)
+data.set(cloudContractPlaybook.id, cloudContractPlaybook)
+
+const repository: PlaybookRepository = {
+  findById(id: string) {
+    const playbook = data.get(id)
+    if (typeof playbook === "undefined") {
+      throw new Error("Contract not found")
+    }
+    return playbook
+  },
+  save(playbook: Playbook) {
+    data.set(playbook.id, playbook)
+  },
+}
+
+export default repository
